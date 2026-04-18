67698848071c085b7346e2f143f277b6ec51d92b
Valiray <vivi@valiray.gay>
2026-04-18 17:42:12 -0500

    fix shit so israeli forge doesnt block my mod anymore

--- src/main/java/mirror/normalasm/api/StacktraceDeobfuscator.java
+++ src/main/java/mirror/normalasm/api/StacktraceDeobfuscator.java
@@ -16,64 +16,28 @@ public final class StacktraceDeobfuscator {
 
     private static Map<String, String> srgMcpMethodMap = null;
 
-    /**
-     * If the file does not exits, downloads latest method mappings and saves them to it.
-     * Initializes a HashMap between obfuscated and deobfuscated names from that file.
-     */
     public static void init(File mappings) {
         if (srgMcpMethodMap != null) {
             return;
         }
-        // Download the file if necessary
-        if (!mappings.exists()) {
-            HttpURLConnection connection = null;
-            try {
-                URL mappingsURL = new URL("https://raw.githubusercontent.com/CleanroomMC/MCPMappingsArchive/master/mcp_stable_nodoc/39-1.12/mcp_stable_nodoc-39-1.12.zip");
-                connection = (HttpURLConnection) mappingsURL.openConnection();
-                connection.setDoInput(true);
-                connection.connect();
-                try (InputStream inputStream = connection.getInputStream()) {
-                    ZipInputStream zipInputStream = new ZipInputStream(inputStream);
-                    ZipEntry entry;
-                    while ((entry = zipInputStream.getNextEntry()) != null) {
-                        if (entry.getName().equals("methods.csv")) {
-                            try (FileOutputStream out = new FileOutputStream(mappings)) {
-                                byte[] buffer = new byte[2048];
-                                int len;
-                                while ((len = zipInputStream.read(buffer)) > 0) {
-                                    out.write(buffer, 0, len);
-                                }
-                            }
-                            break;
-                        }
-                    }
-                    if (entry == null) {
-                        throw new RuntimeException("Downloaded zip did not contain methods.csv");
-                    }
-                }
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            } finally {
-                if (connection != null) {
-                    connection.disconnect();
-                }
-            }
-        }
-        // Read the mapping
         Map<String, String> srgMcpMethodMap = new Object2ObjectOpenHashMap<>();
-        try (Scanner scanner = new Scanner(mappings)) {
-            scanner.nextLine(); // Skip CSV header
-            while (scanner.hasNext()) {
-                String mappingLine = scanner.nextLine();
-                int commaIndex = mappingLine.indexOf(',');
-                String srgName = mappingLine.substring(0, commaIndex);
-                String mcpName = mappingLine.substring(commaIndex + 1, commaIndex + 1 + mappingLine.substring(commaIndex + 1).indexOf(','));
-                srgMcpMethodMap.put(srgName, mcpName);
+        try (InputStream is = StacktraceDeobfuscator.class.getResourceAsStream("/methods.csv")) { // curseforge is stinky 🤮
+            if (is == null) {
+                throw new RuntimeException("methods.csv not found in classpath");
+            }
+            try (Scanner scanner = new Scanner(is)) {
+                scanner.nextLine();
+                while (scanner.hasNext()) {
+                    String mappingLine = scanner.nextLine();
+                    int commaIndex = mappingLine.indexOf(',');
+                    String srgName = mappingLine.substring(0, commaIndex);
+                    String mcpName = mappingLine.substring(commaIndex + 1, commaIndex + 1 + mappingLine.substring(commaIndex + 1).indexOf(','));
+                    srgMcpMethodMap.put(srgName, mcpName);
+                }
             }
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-        // Set the map only if it's successful, to make sure that it's complete
         StacktraceDeobfuscator.srgMcpMethodMap = srgMcpMethodMap;
     }
 
